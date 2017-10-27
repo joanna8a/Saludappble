5574998cb43823dda016256a290a4ed6aadaf928
joanna8a <jdochoam@unal.edu.co>
2017-10-27 15:04:11 -0500

    Dapp contract with structs

---
+++ contracts/Saludappble.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.15;
+contract Saludappble {
+    struct Doctor{
+
+        string dDocument;
+        string public dName;
+        string specialty;
+        address public dAddress;
+      }
+    struct Patient{
+            string pId;
+            string public pName;
+            address public pAddress;
+      }
+    struct MedicalEntry{
+        address author;
+        string date;
+        string body;
+    }
+
+    mapping (uint => Patient) patients;
+    public uint pCount = 0;
+    mapping (uint => Doctor) doctors;
+    public uint dCount = 0;
+
+    function create Patient(string id, string name){
+
+        patients[pCount] = Patient(id, name);
+        pCount++;
+
+    }
+
+    function create Doctor(string id, string name, string speciality){
+
+            doctors[dCount] = Doctor(id, name, speciality);
+            dCount++;
+
+    }
+
+
+
+}
